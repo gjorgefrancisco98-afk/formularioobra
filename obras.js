d31c56c4d068495512ccfef1046427b4ba3ee036
gjorgefrancisco98-afk <g.jorgefrancisco98@gmail.com>
2025-12-21 17:06:27 -0300

    Add files via upload

--- obras.js
+++ obras.js
@@ -1,54 +1,58 @@
-/* BANCO DE DADOS DE OBRAS - FORALL ENGENHARIA
-   Para adicionar uma obra, copie o bloco entre chaves {} e altere os dados.
-   Formato da data: "AAAA-MM-DD" (Ano-Mês-Dia)
-*/
+/* BANCO DE DADOS ATUALIZADO EM 21/12/2025 */
 
 const BANCO_DE_OBRAS = [
     {
-        id: 1,
-        nome: "UBS Florabella",
-        contrato: "042/2025",
-        empresa: "Prefeitura Mun. de Holambra",
-        objeto: "Reforma e Ampliação da Unidade Básica de Saúde Florabella",
-        inicio: "2025-01-15",
-        termino: "2025-08-15"
+        "id": 1,
+        "nome": "UBS Florabella",
+        "contrato": "042/2025",
+        "empresa": "Prefeitura Mun. de Holambra",
+        "objeto": "Reforma e Ampliação da Unidade Básica de Saúde Florabella",
+        "inicio": "2025-01-15",
+        "termino": "2025-08-15"
     },
     {
-        id: 2,
-        nome: "Sede CONDESU Holambra",
-        contrato: "105/2024",
-        empresa: "Consórcio Intermunicipal (CONDESU)",
-        objeto: "Construção da Nova Sede Administrativa",
-        inicio: "2024-11-01",
-        termino: "2025-05-30"
-    },
-    // Para adicionar uma nova obra, copie da vírgula acima até aqui e cole abaixo:
-    {
-        id: 3,
-        nome: "Residencial Jaguariúna",
-        contrato: "Particular/2025",
-        empresa: "Cliente Particular",
-        objeto: "Gerenciamento de construção residencial unifamiliar",
-        inicio: "2025-02-01",
-        termino: "2025-12-20"
+        "id": 2,
+        "nome": "Sede CONDESU Holambra",
+        "contrato": "105/2024",
+        "empresa": "Consórcio Intermunicipal (CONDESU)",
+        "objeto": "Construção da Nova Sede Administrativa",
+        "inicio": "2024-11-01",
+        "termino": "2025-05-30"
     },
     {
-        id: 4,
-        nome: "CNC Tech",
-        contrato: "Particular/2025",
-        empresa: "ANDRE EDUARDO BARBOZA DE LIMA",
-        objeto: "Construção de galpão comercial",
-        inicio: "2025-12-10",
-        termino: "2026-09-10"
+        "id": 3,
+        "nome": "Residencial Jaguariúna",
+        "contrato": "Particular/2025",
+        "empresa": "Cliente Particular",
+        "objeto": "Gerenciamento de construção residencial unifamiliar",
+        "inicio": "2025-02-01",
+        "termino": "2025-12-20"
     },
     {
-        id: 5,
-        nome: "CASA GH",
-        contrato: "Particular/2026",
-        empresa: "Cliente Particular",
-        objeto: "Gerenciamento de construção residencial unifamiliar",
-        inicio: "2026-06-01",
-        termino: "2026-12-20"
+        "id": 4,
+        "nome": "CNC Tech",
+        "contrato": "Particular/2025",
+        "empresa": "ANDRE EDUARDO BARBOZA DE LIMA",
+        "objeto": "Construção de galpão comercial",
+        "inicio": "2025-12-10",
+        "termino": "2026-09-10"
+    },
+    {
+        "id": 5,
+        "nome": "CASA GH",
+        "contrato": "Particular/2026",
+        "empresa": "Cliente Particular",
+        "objeto": "Gerenciamento de construção residencial unifamiliar",
+        "inicio": "2026-06-01",
+        "termino": "2026-12-20"
+    },
+    {
+        "id": 1766347537942,
+        "nome": "Maitê",
+        "empresa": "Mait^R Ferreira francisco",
+        "objeto": "casa",
+        "contrato": "102530",
+        "inicio": "2025-11-30",
+        "termino": "2026-01-10"
     }
-
 ];
